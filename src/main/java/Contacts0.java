6cedc6bfaf75b70faa33ef939cf62756f2b51f69
poppolette <seashanty13.9@gmail.com>
2021-08-27 17:56:35 +0800

    Refactor code

--- src/main/java/Contacts0.java
+++ src/main/java/Contacts0.java
@@ -3,17 +3,16 @@ import java.util.Scanner;
 
 public class Contacts0 {
 
+    public static final int MAXIMUM_LIST_SIZE = 100;
+    public static final String OUTPUT_FIELD_SEPARATOR = "|| ";
+
     public static void main(String[] args) {
         final Scanner SCANNER = new Scanner(System.in);
-        System.out.println("|| ===================================================");
-        System.out.println("|| ===================================================");
-        System.out.println("|| Contacts - Version 0.0");
-        System.out.println("|| Welcome to Contacts!");
-        System.out.println("|| ===================================================");
-        String[][] list = new String[100][3];
+        showGreetingsMessage();
+        String[][] list = new String[MAXIMUM_LIST_SIZE][3];
         int count = 0;
         while (true) {
-            System.out.print("|| " + "Enter command: ");
+            System.out.print(OUTPUT_FIELD_SEPARATOR + "Enter command: ");
             String inputLine = SCANNER.nextLine();
             while (inputLine.trim().isEmpty() || inputLine.trim().charAt(0) == '#') {
                 inputLine = SCANNER.nextLine();
@@ -144,6 +143,15 @@ public class Contacts0 {
                 System.out.println("|| " + m);
             }
         }
+    }
+
+
+    private static void showGreetingsMessage() {
+        System.out.println("|| ===================================================");
+        System.out.println("|| ===================================================");
+        System.out.println("|| Contacts - Version 1.0");
+        System.out.println("|| Welcome to Contacts!");
+        System.out.println("|| ===================================================");
     }
 
 }
